d7f1f5d663cd736ddf4f22c56352fdf60097f939
valeriosalvucci <valerio.salvucci@gmail.com>
2025-02-09 15:41:56 +0900

    bug fix

--- agent.py
+++ agent.py
@@ -15,7 +15,7 @@ load_dotenv()
 
 # model = LiteLLMModel(model_id="gpt-3.5-turbo-0125", api_key=os.environ['OPENAI_API_KEY'])
 # model = LiteLLMModel(model_id="gpt-4o", api_key=os.environ['OPENAI_API_KEY'])
-if os.environ.get('USE_GOOGLE_API',True):
+if os.environ.get('USE_GOOGLE_API',False):
     model = LiteLLMModel(model_id="gemini/gemini-2.0-pro-exp" , api_key=os.environ['GOOGLE_API_KEY'])
 else:
     model = LiteLLMModel(model_id="claude-3-haiku-20240307", api_key=os.environ['ANTHROPIC_API_KEY'])
